bc1044c6c47bb8f275fe90dd021bdab4fe0f10b5
noamcohenntc <noamcohenntc@gmail.com>
2023-10-06 17:54:53 +0300

    done

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -3,9 +3,29 @@
             [domina :as dom]
             [domina.events :as ev]))
 
-(defonce conn
-  (repl/connect "http://localhost:9000/repl"))
-
 (enable-console-print!)
 
-(println "Hello world!")
+(defn arithmetic [a b]
+  (/ (+ a b) 2.0))
+
+(defn geometric [a b]
+  (js/Math.sqrt (* a b)))
+
+(defn harmonic [a b]
+  (/ 2 (+ (/ 1 a) (/ 1 b))))
+
+(defn float-value [id]
+  (.parseFloat js/window (dom/value (dom/by-id id))))
+
+(defn doCal [fun]
+  (fun (float-value "A") (float-value "B")))
+
+(defn calculate [e]
+  (dom/set-text! (dom/by-id "arithmetic")
+                 (doCal arithmetic))
+  (dom/set-text! (dom/by-id "geometric")
+                 (doCal geometric))
+  (dom/set-text! (dom/by-id "harmonic")
+                 (doCal harmonic)))
+(ev/listen! (dom/by-id "calc") "click" calculate)
+
